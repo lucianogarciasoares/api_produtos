e11a7473b672fac6a02e670e976532e0651c899d
lucianojavadev <luciano.garcia.s1989@gmail.com>
2023-02-15 22:06:51 -0300

    Desenvolvimento da API de produtos

---
+++ src/main/java/br/com/cotiinformatica/dtos/PostProdutosDTO.java
@@ -0,0 +1,21 @@
+package br.com.cotiinformatica.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class PostProdutosDTO {
+	
+	private String nome;
+	private Double preco;
+	private Integer quantidade;
+	
+
+}
